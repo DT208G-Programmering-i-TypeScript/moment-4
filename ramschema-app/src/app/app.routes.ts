7f35ca01c11c6cec473fcde6486e68e2ddecde70
Babak Jameei <babak.jameei@outlook.com>
2024-10-27 13:41:23 +0100

    lagt till netlify beroenden

--- ramschema-app/src/app/app.routes.ts
+++ ramschema-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { RamschemaComponent } from './ramschema/ramschema.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: RamschemaComponent },
+  { path: '**', redirectTo: '' }  // Fallback route
+];
